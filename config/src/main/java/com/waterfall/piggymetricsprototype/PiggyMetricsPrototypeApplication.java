64ba31dab128f116e86b6e94da23fed4113c76e3
karnauhvitaliy <karnauhvitaliy@gmail.com>
2018-12-25 19:25:54 +0200

    Added config service microserice module

---
+++ config/src/main/java/com/waterfall/piggymetricsprototype/PiggyMetricsPrototypeApplication.java
@@ -0,0 +1,14 @@
+package com.waterfall.piggymetricsprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggyMetricsPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiggyMetricsPrototypeApplication.class, args);
+	}
+
+}
+
